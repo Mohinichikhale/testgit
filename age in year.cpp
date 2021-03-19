a6f78a4989e713be170ad5e0fea91e1e3f000f14
Mohinichikhale <80941926+Mohinichikhale@users.noreply.github.com>
2021-03-19 12:32:16 +0530

    Add files via upload

---
+++ age in year.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int age_in_year , age_in_sec ;
+	printf("enter age in year\n");
+	scanf("%d",&age_in_year);
+	age_in_sec = 3.156*10*age_in_year ;
+	printf("age in second : %d\n",age_in_sec );
+	
+	
+	
+	
+	return 0 ;
+	 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
