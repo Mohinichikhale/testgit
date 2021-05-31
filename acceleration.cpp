cbe349a40a91167884a092bf667acfe2a8f96bbc
Mohinichikhale <80941926+Mohinichikhale@users.noreply.github.com>
2021-05-31 18:43:35 +0530

    Add files via upload

---
+++ acceleration.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int u , a , v, t ;
+	printf("input initial velocity and acceleration of object \n");
+	scanf("%d %d", &u , &a);
+	printf("enter the time \n");
+	scanf("%d", &t)	;
+	v = u+a*t ;
+	printf("value of velocity :%d\n",v);
+	
+	
+	
+	
+    return 0 ;
+	   
+	
+	
+	
+	
+	
+	
+	
+	
+}
