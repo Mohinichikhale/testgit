e90f2f8cf385981081fad01ed8bd19fe740e66bb
Mohinichikhale <80941926+Mohinichikhale@users.noreply.github.com>
2021-10-10 12:17:32 +0530

    Add files via upload

---
+++ Kilometer.java
@@ -0,0 +1,25 @@
+package TCS;
+
+import java.util.Scanner;
+
+public class Kilometer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("for convertin number from kilometer to meter\n");
+	
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter number in kilometer\n");
+		int a = sc.nextInt();
+		int c = a*1000;
+		System.out.println("the conversion of number from kilometer to meter is :\n");
+		System.out.println(c);
+		
+		
+		
+		
+		
+
+	}
+
+}
