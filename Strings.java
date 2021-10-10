cb97c1d421e8961c1f007f3be21abdd397757738
Mohinichikhale <80941926+Mohinichikhale@users.noreply.github.com>
2021-10-10 12:13:17 +0530

    Add files via upload

---
+++ Strings.java
@@ -0,0 +1,30 @@
+package TCS;
+
+import java.util.Scanner;
+
+public class Strings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//String name = "Mohini";
+		//System.out.println(name);
+		//int value = name.length();
+		//System.out.println(value);
+		//String lstring = name.toLowerCase();
+		//System.out.println(lstring);
+		//String ustring = name.toUpperCase();
+		//System.out.println(ustring);
+		Scanner sc = new Scanner(System.in);
+		System.out.print("enter a string");
+		String str = sc.next();
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
