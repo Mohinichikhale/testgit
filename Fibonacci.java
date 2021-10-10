d8bb174a5d1c70caf179060daa904b122a621a6d
Mohinichikhale <80941926+Mohinichikhale@users.noreply.github.com>
2021-10-10 12:16:33 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,27 @@
+package TCS;
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1 = 0;
+		int n2 = 1;
+		int n3;
+		int i;
+		int count = 10;
+		System.out.println(n1+""+n2);
+		for(i=0;i<=count;++i)
+		{
+			n3=n1+n2;
+			System.out.println(""+n3);
+			n1=n2;
+			n2=n3;
+			
+		}
+		
+		
+		
+
+	}
+
+}
